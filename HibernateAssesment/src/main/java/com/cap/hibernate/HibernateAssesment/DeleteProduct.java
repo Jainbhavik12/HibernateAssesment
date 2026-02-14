873caebafdabdc3253c7e3e316bd32ca86b7f8ca
Jainbhavik12 <114860914+Jainbhavik12@users.noreply.github.com>
2026-02-14 16:01:39 +0530

    Change product ID type from long to int

--- HibernateAssesment/src/main/java/com/cap/hibernate/HibernateAssesment/DeleteProduct.java
+++ HibernateAssesment/src/main/java/com/cap/hibernate/HibernateAssesment/DeleteProduct.java
@@ -5,10 +5,11 @@ import org.hibernate.Session;
 import Entity.Product;
 
 public class DeleteProduct {
-	public void deleteProduct(Session session, long id) {
+	public void deleteProduct(Session session, int id) {
 		RetriveFromDatabase retrive = new RetriveFromDatabase();
 		Product product = retrive.retriveProduct(session, id);
 		session.delete(product);
 		System.out.println("Product deleted successfully");
 	}
 }
+
